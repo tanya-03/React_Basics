5095c08905598f74e7e6f6cdb9f73199422c9e2e
tanya-03 <batratanya03@gmail.com>
2024-12-11 17:05:06 +0530

    react

---
+++ 03tailwindprops/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from './Components/Card';
+
+function App() {
+  return (
+    <div className="App flex items-center justify-center">
+      <Card media="View Me"/>
+      <Card media="Click Me"/>
+    </div>
+  );
+}
+
+export default App;
