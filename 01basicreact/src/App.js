47386023a9c848b7893a78f84fd440ace6930373
tanya-03 <batratanya03@gmail.com>
2024-12-11 17:01:08 +0530

    tailwind

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+    <h1 style={{color:'red'}}>Hello World!!</h1>
+    <h2 style={{backgroundColor:'pink'}}>Bye!!!</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Test.js
@@ -0,0 +1,6 @@
+function Test(){
+    return(
+        <h1>Test</h1>
+    );
+}
+export default Test;
